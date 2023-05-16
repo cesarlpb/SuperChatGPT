d81f60745fe35759451afddd1d6c5d8bd1baaac7
César <cesarlpb@gmail.com>
2023-05-16 20:00:20 +0200

    Código para conectar a la base de datos

--- prompts.php
+++ prompts.php
@@ -1,3 +1,19 @@
 <?php
-    echo "Hola";
+
+// Conexión a la base de datos local
+// Variables de conexión:
+$db_host = "localhost";
+$db_user = "root"; // TODO: en el futuro, crearemos un usuario con permisos
+$db_password = "";
+$db_name = "pruebas";
+
+// Crear conexión
+$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+// Comprobar si hay errores
+if(!$conn){
+    die("Error en la conexión: " . mysqli_connect_error());
+}
+echo "Conexión exitosa!";
+
 ?>
