40cd0ac515852bd4e6eddf04d6b4ff4802df6970
César <cesarlpb@gmail.com>
2023-05-16 20:29:06 +0200

    Bucle: escribir los prompts de la base de datos

--- prompts.php
+++ prompts.php
@@ -14,20 +14,27 @@ $conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 if(!$conn){
     die("Error en la conexión: " . mysqli_connect_error());
 }
-echo "Conexión exitosa!";
+// echo "Conexión exitosa!";
 
 // Query para sacar lista de usuario con id 1:
 $query = "SELECT * FROM prompts";
-$resultado = mysqli_query($conn, $query);
-// Pasamos el objeto a un array:
-$arr = mysqli_fetch_array($resultado); 
+$resultado = mysqli_query($conn, $query); 
 
 // Bucle para avisar si el query funcionó
 
 echo "<h2>Prompts</h2>";
-// Escribir los resultados (normalmente con un bucle):
-echo "id: " . $arr["id"] . "<br>";
-echo "preguntas: " . $arr["preguntas"] . "<br>";
-echo "respuestas: " . $arr["respuestas"] . "<br>";
+
+// Bucle para escribir todos los prompts:
+while($fila = mysqli_fetch_array($resultado, MYSQLI_ASSOC)){
+    echo "<h2>#" . $fila["id"] . "</h2>";
+    echo "preguntas: " . $fila["preguntas"] . "<br>";
+    echo "respuestas: " . $fila["respuestas"] . "<br>";
+}
 // Solo tenemos un dato asi que no ponemos bucle :)
+
+// Liberamos memoria
+mysqli_free_result($resultado);
+
+//Closing the connection
+mysqli_close($conn);
 ?>
